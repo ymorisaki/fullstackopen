859926ad27e277cbc86ae74916ab9499cb600eae
MorisakiYuji <youkourow@yahoo.co.jp>
2022-04-14 12:42:14 +0900

    Fix: 電話帳リストをjsonに変更

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -7,13 +7,15 @@ import Filter from './Filter'
 import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    (async () => {
+      const {data} = await axios.get('http://localhost:3001/persons')
+      setPersons(data)
+    })()
+  }, [])
 
   return (
     <>
