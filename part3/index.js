f21472af56a4fa613ccb421f357768d44a58361a
MorisakiYuji <youkourow@yahoo.co.jp>
2022-05-18 10:18:48 +0900

    Fix: index.js

--- part3/index.js
+++ part3/index.js
@@ -35,7 +35,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
