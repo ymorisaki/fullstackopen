344b65ee619cfe6e1191fafc554816025033af84
MorisakiYuji <youkourow@yahoo.co.jp>
2022-05-24 17:45:50 +0900

    Fix: schemaのバリデート

--- bloglist/models/blog.js
+++ bloglist/models/blog.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose');
 const config = require('../util/config');
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
 })
 
 mongoose.connect(config.MONGODB_URI)
