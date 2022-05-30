e76a1e3157be2b70ad55de1609efd9f2851b4ad5
MorisakiYuji <youkourow@yahoo.co.jp>
2022-05-30 15:29:57 +0900

    Add: loginのpost

--- bloglist/app.js
+++ bloglist/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const blogRouter = require('./controller/blogs')
 const userRouter = require('./controller/users')
+const loginRouter = require('./controller/login')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
