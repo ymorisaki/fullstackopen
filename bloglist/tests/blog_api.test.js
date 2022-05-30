dee755843f96fdf3be6168e532e2788a778d593f
MorisakiYuji <youkourow@yahoo.co.jp>
2022-05-30 18:25:17 +0900

    Fix: chore

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const { set } = require('../app')
 const app = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
