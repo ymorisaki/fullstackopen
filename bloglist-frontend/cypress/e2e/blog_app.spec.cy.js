5983df962f5c9559087f9530a7c0fec9e4f0b1d0
MorisakiYuji <youkourow@yahoo.co.jp>
2022-06-09 18:32:59 +0900

    Add: login test

--- bloglist-frontend/cypress/e2e/blog_app.spec.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.spec.cy.js
@@ -2,10 +2,44 @@ describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
+  })
+
+  it('create user', function () {
     cy.request('POST', 'http://localhost:3003/api/users', {
       username: 'yuji',
       name: 'mori',
       password: 'password'
+    })
+  })
+
+  describe('login', function () {
+    beforeEach(function () {
+      cy.request('POST', 'http://localhost:3003/api/users', {
+        username: 'yuji',
+        name: 'mori',
+        password: 'password'
+      })
+    })
+
+    it('success', function () {
+      cy.get('#input-name').type('yuji')
+      cy.get('#input-password').type('password')
+      cy.get('#button-login').click()
+      cy.contains('yuji logged in')
+    })
+
+    it('invalid name', function () {
+      cy.get('#input-name').type('yuj')
+      cy.get('#input-password').type('password')
+      cy.get('#button-login').click()
+      cy.contains('username or password invalid')
+    })
+
+    it('invalid password', function () {
+      cy.get('#input-name').type('yuji')
+      cy.get('#input-password').type('passwor')
+      cy.get('#button-login').click()
+      cy.contains('username or password invalid')
     })
   })
 
