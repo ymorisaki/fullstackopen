f17295538941a0a38cf4d38bb40e749f07099d98
MorisakiYuji <youkourow@yahoo.co.jp>
2022-06-13 18:38:01 +0900

    Fix: build

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,9 +1,6 @@
-const http = require('http')
 const app = require('./app')
 const config = require('./util/config')
 
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   console.log(`Server running http:localhost:${config.PORT}`)
 })
