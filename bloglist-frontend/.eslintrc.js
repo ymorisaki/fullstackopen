11ca6e35ff22a0b4cc309dceb2f271c0d7ebe9e0
MorisakiYuji <youkourow@yahoo.co.jp>
2022-06-13 17:04:33 +0900

    Fix: unuserd-vas

--- bloglist-frontend/.eslintrc.js
+++ bloglist-frontend/.eslintrc.js
@@ -46,7 +46,8 @@ module.exports = {
     ],
     'no-console': 0,
     'react/prop-types': 0,
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 0
   },
   'settings': {
     'react': {
