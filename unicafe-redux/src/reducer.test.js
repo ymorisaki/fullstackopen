11a91912646021cc6c61d5e83317bb68a1c2f2a5
MorisakiYuji <youkourow@yahoo.co.jp>
2022-06-15 16:40:19 +0900

    Fix: test

--- unicafe-redux/src/reducer.test.js
+++ unicafe-redux/src/reducer.test.js
@@ -1,26 +1,26 @@
 const deepFreeze = require('deep-freeze')
-const counterReducer = (state = {}, action) => {
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return {...state, good: state.good + 1}
     case 'OK':
-      return state
+      return {...state, ok: state.ok + 1}
     case 'BAD':
-      return state
+      return {...state, bad: state.bad + 1}
     case 'ZERO':
-      return state
+      return initialState
     default: return state
   }
+  
 }
 
 describe('unicafe reducer', () => {
-  const initialState = {
-    good: 0,
-    ok: 0,
-    bad: 0
-  }
-
   test('should return a proper initial state when called with undefined state', () => {
     const state = {}
     const action = {
@@ -43,6 +43,53 @@ describe('unicafe reducer', () => {
       good: 1,
       ok: 0,
       bad: 0
+    })
+  })
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD',
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      bad: 1,
+      ok: 0,
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    })
+  })
+
+  test('all reset', () => {
+    const action = {
+      type: 'ZERO',
+    }
+    const state = {
+      good: 1,
+      ok: 1,
+      bad: 1,
+    }
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      bad: 0,
+      ok: 0
     })
   })
 })
