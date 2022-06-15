e80c927eb3888ffc21ea5fce7e5e2359f92b3ac4
MorisakiYuji <youkourow@yahoo.co.jp>
2022-06-15 16:09:20 +0900

    Add unicafe-redux

---
+++ unicafe-redux/src/reducer.test.js
@@ -0,0 +1,48 @@
+const deepFreeze = require('deep-freeze')
+const counterReducer = (state = {}, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      return state
+    case 'OK':
+      return state
+    case 'BAD':
+      return state
+    case 'ZERO':
+      return state
+    default: return state
+  }
+}
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+})
