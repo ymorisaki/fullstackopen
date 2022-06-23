2b4e0ceeac1e9e2f715d05285a3476913760e616
MorisakiYuji <youkourow@yahoo.co.jp>
2022-06-23 13:14:18 +0900

    Add: Router

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Users from './pages/Users'
+import Notes from './pages/Notes'
 
 const App = () => {
+  const padding = {
+    padding: 5,
+  }
+
   return (
-    <>
-      <p>Hello!!!!</p>
-    </>
+    <Router>
+      <div>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2022</i>
+      </div>
+    </Router>
   )
 }
 
