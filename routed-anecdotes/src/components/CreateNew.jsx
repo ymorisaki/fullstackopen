54607c8c86fa7c345549f223df653d375648aea7
MorisakiYuji <youkourow@yahoo.co.jp>
2022-06-28 18:13:33 +0900

    Fix: 演習

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    props.changeNotification(content)
+    props.changeNotification(content.value)
     navigate('/anecdotes')
   }
 
@@ -26,17 +26,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button type="button">reset</button>
       </form>
     </div>
   )
--- routed-anecdotes/src/components/Menu.jsx
+++ routed-anecdotes/src/components/Menu.jsx
@@ -6,6 +6,7 @@ const Menu = () => {
   }
   return (
     <div>
+      <Link style={padding} to="/">home</Link>
       <Link style={padding} to="/anecdotes">anecdotes</Link>
       <Link style={padding} to="/create">create new</Link>
       <Link style={padding} to="/about">about</Link>
