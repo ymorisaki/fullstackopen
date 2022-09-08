601f69d31ff61e3726e4cba6b92d0037f192e1b2
MorisakiYuji <youkourow@yahoo.co.jp>
2022-09-08 11:46:36 +0900

    Add: 演習

---
+++ library-frontend/src/query/query.js
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client"
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+    }
+  }
+`
